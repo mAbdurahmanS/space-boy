be1526406e41c15d469e1d622a7e6c7805ee064e
Muhammad Abdurahman Salsabil <abil7530@gmail.com>
2025-06-03 16:52:51 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     allowedHosts: true,
-    host: '0.0.0.0'
-  }
-})
+    host: "0.0.0.0",
+  },
+  base: "/space-boy/",
+});
